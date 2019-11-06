1d1702f8b079f7a5eb8953bdf5ac6a1b8c15b4f7
HendraLatumeten <hendrawyt34@gmail.com>
2019-11-06 03:54:41 -0800

    file ketiga

---
+++ users/users_layout/navbar.php
@@ -0,0 +1,43 @@
+ <ul class="navbar-nav">
+          <!-- <li class="nav-item  class=" active" ">
+          <a class=" nav-link active " href="home.php"> <i class="ni ni-tv-2 text-primary"></i> Dashboard
+            </a>
+          </li> -->
+          <!-- <li class="nav-item">
+            <a class="nav-link " href="icons.html">
+              <i class="ni ni-planet text-blue"></i> Icons
+            </a>
+          </li> -->
+          <!-- <li class="nav-item">
+            <a class="nav-link " href="maps.html">
+              <i class="ni ni-pin-3 text-orange"></i> Maps
+            </a>
+          </li> -->
+          <li class="nav-item">
+            <a class="nav-link " href="profile.php">
+              <i class="ni ni-single-02 text-yellow"></i> Profil Saya
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="profile.php">
+              <i class="ni ni-bullet-list-67 text-red"></i> Data Pribadi
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="profile.php">
+              <i class="ni ni-ruler-pencil text-info"></i> Test Profiling
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="profile.php">
+              <i class="ni ni-single-copy-04 text-pink"></i>Document
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="profile.php">
+              <i class="ni ni-button-power text-blue"></i>Keluar
+            </a>
+          </li>
+        </ul>
+   
+
